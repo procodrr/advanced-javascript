3b3fad685f39a374235f2e5eb25734f024faac1a
procodrr <76770313+procodrr@users.noreply.github.com>
2022-10-10 21:50:19 +0530

    Add files via upload

---
+++ 01_currying/started-code/script.js
@@ -0,0 +1,21 @@
+function multiply(x, y, z) {
+    return x * y * z
+} 
+
+multiply(2, 3, 5)
+
+multiply(2)(3)(5)
+
+
+
+// function multiplyByTwo(x) {
+//     return x * 2
+// }
+
+// function multiplyByFour(x) {
+//     return x * 4
+// }
+
+// function multiplyByNine(x) {
+//     return x * 9
+// }
